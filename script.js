517e738a545ed1bc46aecaab85e4b35911da89bb
Arthur Lopes Oliveira <Arthur51826596@edu.df.senac.br>
2025-06-12 17:31:51 -0300

    Atualização

--- script.js
+++ script.js
@@ -1,21 +1,46 @@
 function criaCalculadora(){
     return{
-        
         get inicia(){
             this.cliqueBotoes()
         },
+        apagaDisplay(){
+            this.display.value = ''
+        },
+        apagaUm(){
+            this.display.value = this.display.value.slice(0, -1) 
+        },
+        resolveQuestao(operacao){
+            
+            const resultado = eval(`${operacao}`)
+            return resultado
+        },
         display: document.querySelector('.display'),
         cliqueBotoes(){
-            document.addEventListener('click', function(e){
+            document.addEventListener('click', (e)=>{
                 const el = e.target;
                 if (el.classList.contains('btn-num')){
                     this.btnParaDisplay(el.innerText)   
                 }
-            }.bind(this))
+                if(el.classList.contains('btn-numX')){
+                    this.btnParaDisplay('*')
+                }
+                if (el.classList.contains('btn-clear')){
+                    this.apagaDisplay()
+                }
+                if (el.classList.contains('btn-del')){
+                    this.apagaUm()
+                }
+                if (el.classList.contains('btn-eq')){
+                    const resultado = this.resolveQuestao(this.display.value)
+                    this.display.value = resultado
+                }
+                
+            })
         },
         btnParaDisplay(valor){
             this.display.value += valor
-        }
+        },
+        
     }
 }
 
